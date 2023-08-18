b15c6d1539e6279e040fad7e401bf97a80c345c6
etim <etimpaul22@gmail.com>
2023-08-18 03:37:33 +0100

    added function to edit student

--- Contract/Struct.sol
+++ Contract/Struct.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: GPL - 3.0
 
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.5;
 
 contract Mapping {
     struct Student {
@@ -8,22 +8,27 @@ contract Mapping {
         string name;
         uint age;
         bool good;
+        bool exist;
     }
 
-    mapping(address => Student) studentAdd;
+    mapping(address => Student) public studentAdd;
 
     function mappedAddress(
         Student calldata studentDetails,
         address _add
     ) external {
         studentAdd[_add] = studentDetails;
-    }
-
-    function get(address _addr1) public view returns (Student memory) {
-        return studentAdd[_addr1];
+        studentAdd[_add].exist = true;
     }
 
     function removeMap(address _addr1) public {
         delete studentAdd[_addr1];
+    }
+
+    function setSpecific(string memory _name, uint _age, address _add) public {
+        if (studentAdd[_add].exist) {
+            studentAdd[_add].name = _name;
+            studentAdd[_add].age = _age;
+        } else revert("Student doesn't exist");
     }
 }
